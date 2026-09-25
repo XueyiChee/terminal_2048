203a996b76be844d072faffdf384ca7e0478f053
Xueyi Chee <xueyi.school@gmail.com>
2026-09-25 17:54:59 +1000

    End game on termination

--- bin/main.ml
+++ bin/main.ml
@@ -5,24 +5,27 @@ open! Terminal_2048
 let rec loop (game:Game.t) =
   printf "Score: %d\n" (Game.score game);
   print_string (Render.board (Game.board game));
+  match Game.status game with
+  | Terminated -> print_endline "Terminated!"
+  | In_progress -> 
   print_string "$ ";
-  Out_channel.flush Out_channel.stdout;
-  match In_channel.input_line In_channel.stdin with
-  | None ->
-    print_endline "No command passed, try again";
-    loop game
-  | Some raw_str ->
-    match Command.of_string raw_str with
+    Out_channel.flush Out_channel.stdout;
+    match In_channel.input_line In_channel.stdin with
     | None ->
-      print_endline "Invalid command, try again";
+      print_endline "No command passed, try again";
       loop game
-    | Some cmd ->
-      match cmd with
-      | Quit ->
-        print_endline "Quitting"
-      | Direction d ->
-        let game = Game.apply game (Direction d) in
+    | Some raw_str ->
+      match Command.of_string raw_str with
+      | None ->
+        print_endline "Invalid command, try again";
         loop game
+      | Some cmd ->
+        match cmd with
+        | Quit ->
+          print_endline "Quitting"
+        | Direction d ->
+          let game = Game.apply game (Direction d) in
+          loop game
 
 let () = 
   let game = Game.create ~rows:4 ~cols:4 () in
